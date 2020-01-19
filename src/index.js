33e144ec2f1e210e0f3ddcea921d6fd7b3f974a0
Tanya Dolgopolova <35150766+TanyaDolgopolova@users.noreply.github.com>
2020-01-19 19:47:25 +0200

    Stars
    Added stars with animation

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
-import "./styles.css";
+import "./styles.scss";
 import Parallax from "parallax-js";
+import { generateStars } from "./stars";
 
-document.getElementById("app").innerHTML = `
-  <div id="scene" class="scene">
-    <div data-depth="0.2">
-      <div class="starfield"></div>
-    </div>
-    <div data-depth="0.6">
-      <div class="starfield"></div>
-    </div>
-  </div>
-`;
 var scene = document.getElementById("scene");
 var parallaxInstance = new Parallax(scene);
+generateStars();
---
+++ src/stars.js
@@ -0,0 +1,66 @@
+export const generateStars = function() {
+  let firstLayer = document.getElementById("first-layer-stars");
+  let secondLayer = document.getElementById("second-layer-stars");
+  let thirdLayer = document.getElementById("third-layer-stars");
+
+  let iterator = 0;
+  while (iterator <= 500) {
+    let xpositionStar1 = Math.random();
+    let ypositionStar1 = Math.random();
+    let xpositionStar2 = Math.random();
+    let ypositionStar2 = Math.random();
+    let xpositionStar3 = Math.random();
+    let ypositionStar3 = Math.random();
+
+    var positionForStar1 = {
+      x: 2000 * xpositionStar1,
+      y: 700 * ypositionStar1
+    };
+    var positionForStar2 = {
+      x: 2000 * xpositionStar2,
+      y: 700 * ypositionStar2
+    };
+    var positionForStar3 = {
+      x: 2000 * xpositionStar3,
+      y: 700 * ypositionStar3
+    };
+
+    let starType1 = document.createElement("div");
+    starType1.classList.add("star");
+    starType1.classList.add("animated");
+    starType1.classList.add(iterator);
+    starType1.style.cssText =
+      "top:" +
+      positionForStar1.y +
+      "px;left:" +
+      positionForStar1.x +
+      "px;background: whitesmoke;";
+    firstLayer.appendChild(starType1);
+
+    let starType2 = document.createElement("div");
+    starType2.classList.add("star");
+    starType2.classList.add(iterator);
+    starType2.style.cssText =
+      "top:" +
+      positionForStar2.y +
+      "px;left:" +
+      positionForStar2.x +
+      "px;background: white;";
+    secondLayer.appendChild(starType2);
+
+    let starType3 = document.createElement("div");
+    starType3.classList.add("star");
+    starType3.classList.add(iterator);
+    starType3.style.cssText =
+      "top:" +
+      positionForStar3.y +
+      "px;left:" +
+      positionForStar3.x +
+      "px;background: yellow;";
+    thirdLayer.appendChild(starType3);
+
+    iterator++;
+  }
+};
+
+export const animateStars = function(numberOfStarsToAnimate, numberOfLayer) {};
